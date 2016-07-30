6ba5b12a98f14f9b40dfc4df12064a11651e5ea1
David Zielski <davidzielski@gmail.com>
2016-07-30 07:02:35 -0700

    Completed email authentication with Firebase

--- MobileDez Social/SignInVC.swift
+++ MobileDez Social/SignInVC.swift
@@ -13,6 +13,11 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+  @IBOutlet weak var emailField: FancyFieldTextBox!
+  @IBOutlet weak var pwdField: FancyFieldTextBox!
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -43,13 +48,30 @@ class SignInVC: UIViewController {
   func firebaseAuth(_ credential: FIRAuthCredential) {
     FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
       if error != nil {
-        print("DZ: Unable to authenticate with Firebase - \(error)")
+        print("DZ: Unable to authenticate Facebook user with Firebase - \(error)")
       } else {
-        print("DZ: Successfully authenticated with Firebase")
+        print("DZ: Successfully authenticated Facebook user with Firebase")
       }
     })
   }
 
+  @IBAction func signInTapped(_ sender: AnyObject) {
+    if let email = emailField.text, let pwd = pwdField.text {
+      FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+        if error == nil {
+          print("DZ: Email user authenticated with Firebase")
+        } else {
+          FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+            if error != nil {
+              print("DZ: Unable to authenticate user with emailwith Firebase - \(error)")
+            } else {
+              print("DZ: Sussusfully authenticated user with email with Firebase")
+            }
+          })
+        }
+      })
+    }
+  }
   
 }
 

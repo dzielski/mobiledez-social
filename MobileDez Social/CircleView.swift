699e1042ace6c869c549c177084ee3ebd3e7bbb7
David Zielski <davidzielski@gmail.com>
2016-07-30 14:46:03 -0700

    Added feed ui components

--- MobileDez Social/CircleView.swift
+++ MobileDez Social/CircleView.swift
@@ -1,24 +1,27 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  MobileDez Social
 //
-//  Created by David Zielski on 7/29/16.
+//  Created by David Zielski on 7/30/16.
 //  Copyright © 2016 mobiledez. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+  
   override func awakeFromNib() {
-      super.awakeFromNib()
+    super.awakeFromNib()
     
     layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
     layer.shadowOpacity = 0.8
     layer.shadowRadius = 5.0
     layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-    layer.cornerRadius = 2.0
-    
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layer.cornerRadius = self.frame.width / 2
   }
 
 }
--- MobileDez Social/RoundBtn.swift
+++ MobileDez Social/RoundBtn.swift
@@ -1,24 +1,29 @@
 //
-//  FancyView.swift
+//  RoundBtn.swift
 //  MobileDez Social
 //
-//  Created by David Zielski on 7/29/16.
+//  Created by David Zielski on 7/30/16.
 //  Copyright © 2016 mobiledez. All rights reserved.
 //
 
 import UIKit
-
-class FancyView: UIView {
-
+class RoundBtn: UIButton {
+  
   override func awakeFromNib() {
-      super.awakeFromNib()
+    super.awakeFromNib()
     
     layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
     layer.shadowOpacity = 0.8
     layer.shadowRadius = 5.0
     layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-    layer.cornerRadius = 2.0
+    imageView?.contentMode = .scaleAspectFit
     
+  }
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    layer.cornerRadius = self.frame.width / 2
   }
 
 }

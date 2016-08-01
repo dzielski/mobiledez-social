e837155e60a8de0dfa9e25d493ab0d2a2872de77
David Zielski <davidzielski@gmail.com>
2016-07-31 20:20:42 -0700

    Completed Firebase display

--- MobileDez Social/FeedVC.swift
+++ MobileDez Social/FeedVC.swift
@@ -48,9 +48,16 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
     let post = posts[indexPath.row]
-    print("DZ: \(post.caption)")
+   
+    if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+      cell.configureCell(post: post)
+      return cell
+    } else {
+      //should never happen
+      return PostCell()
+    }
     
-    return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+    
   }
   
   
